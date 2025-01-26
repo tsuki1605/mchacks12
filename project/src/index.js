69dbf029c5456913b9ec7c513349f53713abe6d1
danyali84 <lixdanya84@gmail.com>
2025-01-25 20:34:09 -0500

    kachow

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Make sure this file exists
-import App from "./App";
+//import "./index.css";
+import BathroomMap from "./BathroomMap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BathroomMap />
   </React.StrictMode>
 );
