6f63ab9230d94abb99effede07dd0b9f18f5089c
danyali84 <lixdanya84@gmail.com>
2025-01-26 08:58:10 -0500

    started input form

---
+++ project/src/index.js
@@ -0,0 +1,9 @@
+
+document.getElementById('main-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const userInput = document.getElementById('user-input').value;
+    if (userInput) {
+        //redirects page
+        
+    }
+});
